2edac5ed0950622dcd6101d9b814ef6f4950583e
Jaume153 <163338482+Jaume153@users.noreply.github.com>
2024-05-23 14:48:29 +0000

    creo que lo tengo

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -4,7 +4,7 @@ const Jumbotron = () => {
   return (
     <div className="bg-light p-5 mb-4">
       <div className="container">
-        <h1 className="display-4">Bien benido a mi web</h1>
+        <h1 className="display-4">Bienvenido a mi web</h1>
         <p className="lead">Esto es una prueba para hacer una landing page con React</p>
         <a className="btn btn-primary btn-lg" href="#learn-more" role="button">Pulsa el boton pero no hace nada</a>
       </div>
