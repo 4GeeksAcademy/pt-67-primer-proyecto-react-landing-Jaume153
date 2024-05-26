a76bdf796dd99a9d2e78986e536e50effc2bbc65
Jaume153 <163338482+Jaume153@users.noreply.github.com>
2024-05-26 08:22:27 +0000

    solucionado el problema del footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3 mt-auto fixed-bottom">
+    <footer className="bg-dark text-white text-center py-3 mt-auto">
       <p> Hecho con ❤️ por Jaume153. Para <a href="https://www.4geeksacademy.com">4Geeks Academy </a> </p>
     </footer>
   );
